e2a4118cf8b80616f29c846c60df6fcd6c610ad4
julietvt <freshitpt@gmail.com>
2021-05-29 13:57:51 +0300

    socket.io: server was created

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,25 +1,19 @@
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
 const app = express();
-const expressWs = require('express-ws')(app);
-const allWss = expressWs.getWss('/');
-app.use(express.json());
+const server = http.Server(app);
+const io = socketIO(server);
 
-//сообщения
-const messages = [];
+const router = require('./router');
+app.use(router);
 
-app.ws('/', function (ws, req) {
-  //работаем с сообщениями
-  ws.send(JSON.stringify(messages));
-  ws.on('message', function (msg) {
-    console.log(msg);
-    // добавляем сообщение
-    messages.push(msg);
-    // оповещаем всех клиентов
-    allWss.clients.forEach(function (client) {
-      client.send(JSON.stringify([msg]));
-    });
+io.on('connection', function (socket) {
+  socket.on('test', (data, options) => {
+    console.log('data:', data);
+    console.log('options:', options);
   });
-  console.log('socket', req.testing);
 });
 
 const PORT = process.env.PORT || 3000;
