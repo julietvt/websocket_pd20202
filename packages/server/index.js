0b9f2229fa3c6abadf9c3dd2535e17dc83b84959
julietvt <freshitpt@gmail.com>
2021-05-29 13:08:13 +0300

    server for connection with many clients

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 const expressWs = require('express-ws')(app);
+
+const allWss = expressWs.getWss('/');
+
 app.use(express.json());
+
 app.ws('/', function (ws, req) {
   ws.on('message', function (msg) {
     console.log(msg);
+    // оповещаем всех клиентов
+    allWss.clients.forEach(function (client) {
+      client.send(msg);
+    });
   });
   console.log('socket', req.testing);
 });
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
