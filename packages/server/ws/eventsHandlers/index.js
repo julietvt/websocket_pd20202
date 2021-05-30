8554b9990d9efb5b8c0c97ea5685dbab6ad766c2
julietvt <freshitpt@gmail.com>
2021-05-30 10:44:14 +0300

    socket.io: server socket was update: add router, handshake, eventsHandlers

---
+++ packages/server/ws/eventsHandlers/index.js
@@ -0,0 +1,14 @@
+module.exports.testHandler = (data, options) => {
+  console.log('data: ', data);
+  console.log('options: ', options);
+};
+/*
+module.exports.messageHandler = (data, options) => {
+  console.log('data: ', data);
+  console.log('options: ', options);
+};
+module.exports.messageHandler = (room, message) => {
+  console.log('room: ', data);
+  console.log('message: ', options);
+};
+*/
---
+++ packages/server/ws/index.js
@@ -0,0 +1,10 @@
+const { testHandler } = require('./eventsHandlers');
+module.exports = function connectionHandler(socket) {
+  //рукопожатие
+  const handshake = socket.handshake;
+  console.log('handshake:::');
+  console.dir(handshake);
+
+  socket.on('test', testHandler);
+  socket.on('message', messageHandler);
+};
