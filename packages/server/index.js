5b55f4c4e3f713cad13d281b22ddb11980582b93
julietvt <freshitpt@gmail.com>
2021-05-30 11:29:44 +0300

    add cors

--- packages/server/index.js
+++ packages/server/index.js
@@ -2,13 +2,28 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 const connectionHandler = require('./ws');
+var cors = require('cors');
 const app = express();
+app.use(cors());
+app.use(express.json());
+const corsOpt = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
 const server = http.Server(app);
-const io = socketIO(server);
+//const io = socketIO(server);
+const io = socketIO(server, { corsOpt });
 
 const router = require('./router');
 const { connect } = require('./router');
 app.use(router);
+
 io.on('connection', connectionHandler);
+io.on('disconnect', (reason) => {
+  console.log(reason);
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
