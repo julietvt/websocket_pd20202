5e297d78bd93b5e5f2fa373b541ccc3277f9816a
julietvt <freshitpt@gmail.com>
2021-05-30 13:33:06 +0300

    provate chat. react components were created

---
+++ packages/client/src/components/UserList/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const UsersList = ({ users, currentUser, onSelect }) => {
+  return (
+    <ul>
+      {users.map((u) => (
+        <li onClick={() => onSelect(u)} key={u}>
+          {' '}
+          {u}{' '}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default UsersList;
